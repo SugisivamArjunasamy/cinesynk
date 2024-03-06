14749c28a3139b193ecc78dd6e6e0f218274d635
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2024-03-06 14:38:21 +0530

    nave bar

--- cinesynk/App/urls.py
+++ cinesynk/App/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view , profile
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('profile', profile, name='profile'),
 ]
