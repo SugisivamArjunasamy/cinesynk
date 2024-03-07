ef14598d8663751dd2f7d85ed9f69b6a6fe0a424
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2024-03-07 19:39:20 +0530

    Added Proffesional Users Modal

--- cinesynk/App/models.py
+++ cinesynk/App/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class ProfessionalUser(models.Model):
+    name = models.CharField(max_length=512)
+    email = models.EmailField()
+    password = models.CharField(max_length=512)
+    about = models.TextField()
+    profile_img = models.TextField(max_length=512, default='')
+    experience = models.CharField(max_length = 125)
+    location = models.CharField(max_length = 125)
+    user_type = models.CharField(max_length=125,  default='')
+    movies_worked = ArrayField(
+        models.JSONField(blank=True, null=True),
+        default=list,
+        blank=True,
+        null=True
+    )
+    posts = ArrayField(
+        models.JSONField(blank=True, null=True),
+        default=list,
+        blank=True,
+        null=True
+    )
--- cinesynk/cinesynk/settings.py
+++ cinesynk/cinesynk/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'cinesynk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': "cinesynk",
+       'USER': 'postgres',
+       'PASSWORD': 'praanesh1981',
+       'HOST': '127.0.0.2',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
